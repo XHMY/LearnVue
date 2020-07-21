e29279951b154ed32126eb77debcc40c59988d9e
Yokey <zyf6@outlook.com>
2020-07-21 10:11:32 +0800

    创建项目

---
+++ myvue/src/router/index.js
@@ -0,0 +1,49 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import layout from '@/layout/index'
+// import index from '@/components/index2'
+
+
+Vue.use(Router)
+
+/* Layout */
+//懒加载 webpack
+//const layout = r => require.ensure([], () => r(require('@/layout/index')), 'layout');
+
+
+export const constantRoutes = [
+    // {
+    //     path: '/login',
+    //     component: login,
+    //     // hidden: true
+    // },
+    {
+        path: '/manage',
+        component: layout
+        // hidden: true
+    }
+    //   {
+    //     path:'/example',
+    //     component:system
+    //   }
+
+
+
+
+]
+//function（）
+
+const createRouter = () => new Router({
+    // mode: 'history', // require service support
+    // scrollBehavior: () => ({ y: 0 }),
+    routes: constantRoutes
+})
+
+const router = createRouter()
+
+// export function resetRouter() {
+//   const newRouter = createRouter()
+//   router.matcher = newRouter.matcher // reset router
+// }
+
+export default router
